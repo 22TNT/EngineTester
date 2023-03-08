c176804597d5f5308b3dabe5fa310a453208e062
22TNT <tnt22@protonmail.com>
2023-03-08 19:46:50 +0700

    Finish all InternalCombustionEngine methods

--- EngineTester/Engine.cs
+++ EngineTester/Engine.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace EngineTester
 {
+    /// <summary>
+    /// Abstract class for an Engine.
+    /// </summary>
     abstract class Engine
     {
-        double _inertia { get; set; } // I - moment of intertia (kg*m^2)
-        double _tempEnvinroment { get; set; } //Tenvironment - environment temperature (C)
-        double _tempEngine { get; set; } // Tengine - engine temperature (C)
-        double _tempOverheat { get; set; } // Toverheat - overheating temperature (C)
-        double _ratioCooling { get; set; } // C - ratio of cooling effectiveness based on engine temp and environment temp
+        public double _inertia { get; set; } // I - moment of intertia (kg*m^2)
+        public double _tempEnvinroment { get; set; } //Tenvironment - environment temperature (C)
+        public double _tempEngine { get; set; } // Tengine - engine temperature (C)
+        public double _tempOverheat { get; set; } // Toverheat - overheating temperature (C)
+        public double _ratioCooling { get; set; } // C - ratio of cooling effectiveness based on engine temp and environment temp
 
         public Engine(double inertia, double tempOverheat, double coolingRatio, double tempEnvinroment)
         {
