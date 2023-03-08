2bc66306bc0ddc178819a0e4176685098dfd4282
22TNT <tnt22@protonmail.com>
2023-03-08 19:31:44 +0700

    Добавьте файлы проекта.

---
+++ EngineTester/Engine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineTester
+{
+    abstract class Engine
+    {
+        double _inertia { get; set; } // I - moment of intertia (kg*m^2)
+        double _tempEnvinroment { get; set; } //Tenvironment - environment temperature (C)
+        double _tempEngine { get; set; } // Tengine - engine temperature (C)
+        double _tempOverheat { get; set; } // Toverheat - overheating temperature (C)
+        double _ratioCooling { get; set; } // C - ratio of cooling effectiveness based on engine temp and environment temp
+
+        public Engine(double inertia, double tempOverheat, double coolingRatio, double tempEnvinroment)
+        {
+            _inertia = inertia;
+            _tempOverheat = tempOverheat;
+            _ratioCooling = coolingRatio;
+            _tempEnvinroment = tempEnvinroment;
+            _tempEngine = _tempEnvinroment;
+        }
+
+        public abstract void EngineHeatUp();
+        public abstract void EngineCooling();
+        public abstract double Acceleration(); 
+    }
+}
